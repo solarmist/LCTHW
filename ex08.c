6707095de115c0d5eeb66eb86bc99f2a66389207
Joshua Olson <jolson@machinezone.com>
2012-09-23 11:48:53 -0700

    Added size of long

--- ex08.c
+++ ex08.c
@@ -2,17 +2,18 @@
 
 int main(int argc, char *argv[])
 {
-  int areas[] = {10, 12, 13, 14, 20};
-  char name[] = "Joshua";
   char full_name[] = {
     'J', 'o', 's', 'h', 'u', 'a',
     ' ', 'D', '.', ' ',
     'O', 'l', 's', 'o', 'n', '\0'
   };
+  int areas[] = {10, 12, 13, 14, 20};
+  char name[] = "Joshua";
 
   // WARNING: On some systems you may have to change the
   // %ld in this code to a %u since it will use unsigned ints
   printf("The size of an int: %ld\n", sizeof(int));
+  printf("The size of an long: %ld\n", sizeof(long));
   printf("The size of areas (int[]): %ld\n",
 	 sizeof(areas));
   printf("The number of ints in areas: %ld\n",
