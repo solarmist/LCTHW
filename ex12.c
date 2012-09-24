78fb7a1c820f1d5fdc3d3561b57d6db2db6fbd8a
Joshua Olson <joshua.olson@gmail.com>
2012-09-24 10:50:13 -0700

    Ex11 -13

--- ex12.c
+++ ex12.c
@@ -2,6 +2,20 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
+  int i = 0;
+
+  if(argc == 1) {
+    printf("You only have no arguments. You suck.\n");
+  } else if (argc > 1 && argc < 4) {
+    printf("here's your arguments:\n");
+
+    for(i = 0; i < argc; i++) {
+      printf("%s ", argv[i]);
+    }
+    printf("\n");
+  } else {
+    printf("You have too many arguments. You suck.\n");
+  }
 
   return 0;
 }
--- ex13.c
+++ ex13.c
@@ -2,6 +2,48 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
+  if(argc !=2) {
+    printf("ERROR: You need exactly one argument.\n");
+    return 1;
+  }
+
+  int i = 0;
+  for(char letter = argv[1][i]; argv[1][i] != '\0'; i++, letter=argv[1][i]) {
+    if (letter > 'Z'){
+	letter -= 32;
+    }
+
+    switch(letter) {
+    case 'A':
+      printf("%d: '%c'\n", i, letter);
+      break;
+
+    case 'E':
+      printf("%d: '%c'\n", i, letter);
+      break;
+
+    case 'I':
+      printf("%d: '%c'\n", i, letter);
+      break;
+
+    case 'O':
+      printf("%d: '%c'\n", i, letter);
+      break;
+
+    case 'U':
+      printf("%d: '%c'\n", i, letter);
+      break;
+
+    case 'Y':
+      if (i > 2) {
+	// it's only sometimes Y
+	printf("%d: '%c'\n", i, letter);
+      }
+      break;
+    default:
+      printf("%d: %c is not a vowel\n", i, letter);
+    }
+  }
 
   return 0;
 }
