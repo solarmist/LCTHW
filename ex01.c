b92864b55937be20f8d31bb675d6996e24504ae5
Joshua Olson <joshua.olson@gmail.com>
2017-08-12 17:54:36 -0700

    Excercise 1v2

--- ex01.c
+++ ex01.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/* This is a comment. */
+int main(int argc, char *argv[])
 {
+  int distance = 100;
+
+  // This is also a comment
+  printf("You are %d miles away.\n", distance);
   puts("Hello World");
   puts("It's been a while since I've written any C.");
   puts("It feels good to have a compiler again.");
